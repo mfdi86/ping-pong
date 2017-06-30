b0bfab7d2e5606b708def82e457a24ba2282aa17
mdc <maria.f.delcastillo@gmail.com>
2017-06-29 18:35:59 -0700

    get up to last page of monday lesson 'exporting functions' section

--- build/js/app.js
+++ build/js/app.js
@@ -1,20 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-//Front-End Logic
-var Calculator = require('./../js/pingpong.js').calculatorModule;
+exports.apiKey = "http://api.openweathermap.org/data/2.5/weather?q=Portland,uk&appid=70175d81a8b1f647629c6728fd06435b";
 
-$(document).ready(function() {
-  $('#ping-pong-form').submit(function(event) {
-    event.preventDefault();
-    var goal = $('#goal').val();
-    var simpleCalculator = new Calculator("hot pink");
-    var output = simpleCalculator.pingPong(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
-  });
-});
-
-},{"./../js/pingpong.js":2}],2:[function(require,module,exports){
+},{}],2:[function(require,module,exports){
 //Back-End Logic//
 
 function Calculator(skinName) {
@@ -39,4 +26,60 @@ Calculator.prototype.pingPong = function(goal) {
 
 exports.calculatorModule = Calculator;
 
-},{}]},{},[1]);
+},{}],3:[function(require,module,exports){
+function Weather(){
+}
+
+Weather.prototype.getWeather = function() {
+  console.log("Hi! I'm a weather object.");
+}
+
+exports.weatherModule = Weather;
+
+},{}],4:[function(require,module,exports){
+//Front-End Logic
+var Calculator = require('./../js/pingpong.js').calculatorModule;
+
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    var goal = $('#goal').val();
+    var simpleCalculator = new Calculator("hot pink");
+    var output = simpleCalculator.pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
+
+$(document).ready(function(){
+  $('#signup').submit(function(event){
+    event.preventDefault();
+  var email = $('#email').val();
+  $('#signup').hide();
+  $('#solution').prepend('<p>Thank you, ' + email + ' has been added to our list!</p>');  
+  });
+});
+
+var apiKey = require('./../.env').apiKey;
+var Weather = require('./../js/weather.js').weatherModule;
+
+$(document).ready(function() {
+  var currentWeatherObject = new Weather();
+  currentWeatherObject.getWeather();
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+     .then(function(response) {
+         $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+      })
+
+      .fail(function(error) {
+        $('.showWeather').text(error.responseJSON.message);
+       });
+  });
+});
+
+},{"./../.env":1,"./../js/pingpong.js":2,"./../js/weather.js":3}]},{},[4]);
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,65 @@
+var del = require('del');
+var utilities = require('gulp-util');
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var buildProduction = utilities.env.production;
 
-gulp.task('jsBrowserify', function() {
-  return browserify({ entries: ['./js/pingpong-interface.js'] })
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+  gulp.watch(['*.html'], ['htmlBuild']);
+});
+
+
+gulp.task('htmlBuild', function() {
+  browserSync.reload();
+});
+
+gulp.task("clean", function(){
+  return del(['build', 'tmp']);
+});
+
+gulp.task("build", ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+});
+
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
+});
+
+
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
